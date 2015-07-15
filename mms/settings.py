fdf06091b64a534fd6ee2f020c9280ec26c1a96e
amandegar <amandegar@gmail.com>
2015-07-15 15:54:52 +0430

    add humanize

--- mms/settings.py
+++ mms/settings.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.humanize',
     'accounts',
     'polymorphic',
     #'django_jalali',
