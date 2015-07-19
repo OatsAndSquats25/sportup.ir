852309b8e9b56e5f0e6519be106dd887d6d5d206
amandegar <amandegar@gmail.com>
2015-07-19 22:36:44 +0430

    accounts: userprofile, database and views... but dashboard needs update for different user.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from registration.signals import user_registered
 
-# Create your models here.
+#---------------------------------------------------------------------------------------------------
+class userProfile(models.Model):
+    user        = models.OneToOneField(User)
+    photo       = models.ImageField(_("Photo"), null=True, blank=True)
+    nid         = models.CharField(_("National Identity"), max_length=10, null=True, blank=True)
+    insurance   = models.CharField(_("Sport`s Insurance Number"), max_length=10, null=True, blank=True)
+    cellPhone   = models.CharField(_("Cell Phone"), max_length=11, null=True, blank=True, help_text=_("example: 09121231234"))
+    landline    = models.CharField(_("Landline"), max_length=12, null=True, blank=True, help_text=_("With area code - example: 02122334455"))
+    postalcode  = models.CharField(_("Postal Code"), max_length=10, null=True, blank=True)
+    address     = models.CharField(_("Address"), max_length=400, null=True, blank=True)
+
+    class Meta:
+        permissions = (
+            ("profile_is_update", "This permission gives to the user who has updated his/her profile"),
+        )
+
+    def isUpdate(self):
+        if self.user.first_name and self.user.last_name and self.photo and self.nid and self.cellPhone and self.postalcode:
+            return True
+        else:
+            return False
+#---------------------------------------------------------------------------------------------------
+def create_user_profile(**kwargs):
+    userProfile.objects.get_or_create(user=kwargs['user'])
+#---------------------------------------------------------------------------------------------------
+user_registered.connect(create_user_profile)
+#---------------------------------------------------------------------------------------------------
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render,redirect
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, DetailView, UpdateView
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse, Http404
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 import importlib
 
 from registration.backends.simple.views import RegistrationView
-from registration.forms import RegistrationForm
-from django import forms
 
-# -----------------------------------------------------------------------
-class MyRegistrationForm(RegistrationForm):
-    next    = forms.CharField(widget=forms.HiddenInput)
+from forms import MyRegistrationForm
+from models import userProfile
+
+User = get_user_model()
 # -----------------------------------------------------------------------
 class MyRegistrationView(RegistrationView):
     form_class = MyRegistrationForm
@@ -18,6 +23,7 @@ class MyRegistrationView(RegistrationView):
         return {'next': self.request.GET.get('next',self.request.POST.get('next','/'))}
 
     def get_success_url(self, request, user):
+        messages.info(request, _("You are successfuly registered."))
         return request.POST.get('next','/')
 # -----------------------------------------------------------------------
 class dashboard(View):
@@ -26,9 +32,14 @@ class dashboard(View):
         cls = self.request.GET.get('cls',None)
 
         if app == None or cls == None:
-            app = 'accounts'
-            cls = 'homePage'
-
+            # app = 'accounts'
+            # cls = 'homePage'
+            # if request.user.has_permission():
+                app = 'program'
+                cls = 'programList'
+            # else:
+            #     app = 'enroll'
+            #     cls = 'enrollList'
         try:
             mod_name = app + '.dashboard'
             mod = importlib.import_module(mod_name)
@@ -38,25 +49,31 @@ class dashboard(View):
 
         return HttpResponse(clas().generate(request))
 # -----------------------------------------------------------------------
-# class dashboard(TemplateView):
-#     template_name = 'dashboard/dashboard.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(dashboard, self).get_context_data(**kwargs)
-#         app = self.request.GET.get('app' ,None)
-#         cls = self.request.GET.get('cls',None)
-#
-#         if app == None or cls == None:
-#             app = 'accounts'
-#             cls = 'homePage'
-#
-#         try:
-#             mod_name = app + '.dashboard'
-#             mod = importlib.import_module(mod_name)
-#             clas = getattr(mod, cls)
-#         except:
-#             return Http404
-#
-#         context['body'] = clas().generate()
-#         return context
+class profileView(DetailView):
+    template_name = 'accounts/profile_detail.html'
+    model = userProfile
+
+    def get_object(self):
+        return self.model.objects.get(user = self.request.user)
+# -----------------------------------------------------------------------
+class profileUpdate(UpdateView):
+    template_name = 'accounts/profile_update.html'
+    model = userProfile
+    fields = ['photo', 'nid','insurance', 'cellPhone', 'landline', 'address', 'postalcode' ]
+
+    # def get_queryset(self):
+    def get_object(self):
+        return self.model.objects.get(user = self.request.user)
+
+    def get_success_url(self):
+        content_type = ContentType.objects.get_for_model(userProfile)
+        permission = Permission.objects.get(content_type=content_type, codename='profile_is_update')
+
+        if self.object.isUpdate() and not self.request.user.has_perm('accounts.profile_is_update'):
+            self.request.user.user_permissions.add(permission)
+        elif not self.object.isUpdate() and self.request.user.has_perm('accounts.profile_is_update'):
+            self.request.user.user_permissions.remove(permission)
+
+        messages.info(self.request, _("Your profile updated successfully."))
+        return reverse('profileUpdate')
 # -----------------------------------------------------------------------
