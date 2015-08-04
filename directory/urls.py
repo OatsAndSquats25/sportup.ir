c964df60c827c59db5e3ec5ef5addcfb8d20f4e8
amandegar <amandegar@gmail.com>
2015-08-04 16:05:15 +0430

    fixed base.html club register button change directory url priority add never_cache to directory listall

--- directory/urls.py
+++ directory/urls.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
-
+from django.views.decorators.cache import never_cache
 from django.conf.urls import patterns, url
 from directory import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.listAllItems.as_view(), name='directoryListAllItems'),
-        url(r'^(?P<slug>.*)/$', views.itemDetail.as_view(), name='directoryItemDetail'),
+        url(r'^$', never_cache(views.listAllItems.as_view()), name='directoryListAllItems'),
         url(r'^register/$', views.clubRegistration.as_view(), name='directoryRegistration'),
+        url(r'^(?P<slug>.*)/$', views.itemDetail.as_view(), name='directoryItemDetail'),
         )
