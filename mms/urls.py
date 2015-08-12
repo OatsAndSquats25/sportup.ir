c18be00da6cf2dfdb1d927eded3b4f84467b1ed9
amandegar <amandegar@gmail.com>
2015-08-12 15:49:18 +0430

    comment webmaster and add Httresponse header

--- mms/urls.py
+++ mms/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.static import directory_index
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 admin.autodiscover()
 
@@ -10,8 +11,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mms.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^google9f49bb0345edbd3d\.html$', lambda r: HttpResponse("google-site-verification: google9f49bb0345edbd3d.html", mimetype="text/plain")),
+#    url(r'^google9f49bb0345edbd3d\.html$', lambda r: HttpResponse("google-site-verification: google9f49bb0345edbd3d.html", mimetype="text/plain")),
     url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: ", mimetype="text/plain")),
     url(r'^accounts/', include("accounts.urls")),
     url(r'^noadmin/', include(admin.site.urls)),
