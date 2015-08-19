9397ba2df23b1fd0ef95b277ed8dc1c0d38c8711
amandegar <amandegar@gmail.com>
2015-08-19 15:13:59 +0430

    agreemtn remaindays admin

--- agreement/admin.py
+++ agreement/admin.py
@@ -4,6 +4,6 @@ from agreement import models
 #import django_jalali.admin as jadmin
 
 class agreementAdmin(admin.ModelAdmin):
-    list_display = ('title','clubKey', 'user', 'status', 'agreementStatus','publish_date','expiry_date')
+    list_display = ('title','clubKey', 'user', 'status', 'agreementStatus','publish_date','expiry_date','remainDays')
 
 admin.site.register(models.agreement, agreementAdmin)
