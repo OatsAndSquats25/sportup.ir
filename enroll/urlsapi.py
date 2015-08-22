3533453579307e716093a37c2790bfdba10773e4
amandegar <amandegar@gmail.com>
2015-08-22 18:45:26 +0430

    enroll/session/list/clug agreement  url

--- enroll/urlsapi.py
+++ enroll/urlsapi.py
@@ -11,7 +11,7 @@ import views
 urlpatterns = patterns('',
     url(r'^enroll/session/(?P<club>\d+)/(?P<week>\d+)/(?P<id>\d+)/$',views.enrollSession.as_view(), name="Enroll_session"),
     url(r'^enroll/session/list/$',views.enrollSessionList.as_view(), name="Enroll_session_list_mine"),
-    url(r'^enroll/session/list/club/$',views.enrollSessionListClub.as_view(), name="Enroll_session_list_club"),
+    url(r'^enroll/session/list/club/(?P<agreement>\d+)/$',views.enrollSessionListClub.as_view(), name="Enroll_session_list_club"),
 )
 
 # urlpatterns += router.urls
