5d63c29feca0f6785ee6c86c2dd84b9d22d615e7
amandegar <amandegar@gmail.com>
2015-08-24 21:14:01 +0430

    show program admin

--- program/admin.py
+++ program/admin.py
@@ -3,10 +3,10 @@ from django.utils.translation import ugettext_lazy as _
 #import django_jalali.admin.filterspecs
 #import django_jalali.admin as jadmin
 
-# from .models import programDefinition
+from models import programDefinition
 # ----------------------------------------------------
-# class programDefinitionAdmin(admin.ModelAdmin):
-#     list_display = ('title',)
-#
-# admin.site.register(programDefinition, programDefinitionAdmin)
+class programDefinitionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+admin.site.register(programDefinition, programDefinitionAdmin)
 # ----------------------------------------------------
