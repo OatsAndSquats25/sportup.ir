451b6249a70c2da18d18da1c3e8e501f46d621db
amandegar <amandegar@gmail.com>
2015-10-04 11:33:02 +0330

    club register form bug fixed

--- flatpages/forms.py
+++ flatpages/forms.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+from django.core.validators import RegexValidator
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
@@ -6,4 +8,4 @@ class clubRegisterForm(forms.Form):
     clubName    = forms.CharField(label=_("Club"))
     ownerName   = forms.CharField(label=_("last name"))
     contact     = forms.CharField(label=_("Cell Phone"))
-    email       = forms.CharField(label=_("Email"), required=False)
+    email       = forms.EmailField(label=_("Email"), required=False)
--- flatpages/views.py
+++ flatpages/views.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import FormView
 from django.utils.translation import ugettext_lazy as _
@@ -9,6 +10,7 @@ from forms import clubRegisterForm
 class clubRegister(FormView):
     form_class = clubRegisterForm
     template_name = "flatpages/clubRegister.html"
+    success_url = "/"
 
     def form_valid(self, form):
         message = "{name} / {email} said: ".format( name=form.cleaned_data.get('clubName'),
@@ -20,5 +22,5 @@ class clubRegister(FormView):
                   from_email='noreply@sportup.ir',
                   recipient_list=["info@sportup.ir",""],)
 
-        messages.info(self.request,"Thank you for filling the form. Your information has beed send to sales department.")
+        messages.info(self.request,_("Thank you for filling the form. Your information has beed send to sales department."))
         return super(clubRegister, self).form_valid(form)
