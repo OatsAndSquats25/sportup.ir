65c72938d8f1f4f733824a2d42e2c89bd67e4a89
amandegar <amandegar@gmail.com>
2015-10-28 09:20:06 +0330

    finance url, change pay to new view

--- finance/urls.py
+++ finance/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls import patterns, url
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required, permission_required
 
-from finance.views import checkout,fianceDeleteItem,paymentResponse,testGateway
+from finance.views import checkout, fianceDeleteItem, paymentResponse, testGateway, checkoutPay
 
 urlpatterns = patterns('',
     url(r'^$', login_required(checkout.as_view()), name='checkoutURL'),
-    url(r'^pay/$', login_required(checkout.as_view()), {'command':'pay'}, name='checkoutPayURL'),
+    url(r'^pay/$', login_required(checkoutPay.as_view()), name='checkoutPayURL'),
     url(r'^deleteitem/(?P<pk>\d+)/$', login_required(fianceDeleteItem.as_view()), name='checkoutDeleteItem'),
     url(r'^paymentresponse/(?P<gateway>.*)/$', csrf_exempt(paymentResponse.as_view()), name='financePaymentResponseURL1'),
     url(r'^paymentresponse/(?P<gateway>.*)/(?P<additional>.*)$', csrf_exempt(paymentResponse.as_view()), name='financePaymentResponseURL2'),
