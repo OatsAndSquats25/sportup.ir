cd25bf2ad63d10ac9695d1ccfedd5d6ee8bf5853
amandegar <amandegar@gmail.com>
2015-10-28 09:37:08 +0330

    finance gateway creditpay.py implemented

---
+++ finance/payment/creditpay.py
@@ -0,0 +1,37 @@
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.contrib import messages
+
+
+from credit.models import userCredit
+from finance.models import transaction
+#---------------------------------------------------------------------------------------
+def paymentRequest(request, InvoiceInst):
+
+    amount = InvoiceInst.amount
+    validCredits = userCredit.objects.active().filter(user = request.user)
+    for credit in validCredits:
+        if amount == 0:
+            break
+        else:
+            if credit.value > amount:
+                credit.value -= amount
+                amount = 0
+            else:
+                credit.value = 0
+                amount -= credit.value
+            credit.save()
+
+    transaction.objects.create(invoiceKey = InvoiceInst,
+                               ref_id='credit',
+                               amount=InvoiceInst.amount,
+                               user=request.user)
+
+    messages.info(request, _("Your payment has been done by your credit."))
+    return reverse('checkoutURL')
+#---------------------------------------------------------------------------------------
+def paymentResponse(request):
+    pass
+    # return{ 'status' : True, 'invoiceId' : transInst.invoiceKey.id }
+#---------------------------------------------------------------------------------------
