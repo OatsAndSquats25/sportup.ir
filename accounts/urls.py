82d564946b3d59a28a457aed6ea65bd3d4fa8399
amandegar <amandegar@gmail.com>
2015-11-04 16:02:31 +0330

    campaign with credit template, view, email etc... full pack

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 
-from views import dashboard, profileUpdate, loginRegister, emailTest, smsTest, dashboardSelector, printForUser
+from views import dashboard, profileUpdate, loginRegister, emailTest, smsTest, dashboardSelector, printForUser, loginRegisterCampaign
 
 urlpatterns = patterns('',
-
-    url(r'^login/$', loginRegister.as_view(), name='accounts_login_register'),
-    url(r'^print/(?P<pk>\d+)/$', login_required(printForUser.as_view()), name='print'),
-    url(r'^dashboard/$', login_required(dashboardSelector.as_view()), name='dashboardURL'),
-    url(r'^profile/$', login_required(profileUpdate.as_view()), name='profileUpdate'),
-    # url(r'^', include('registration.backends.simple.urls')),
-    url(r'^', include('django.contrib.auth.urls')),
-    url(r'^email/$', login_required(emailTest.as_view()), name='emailURL'),
-    url(r'^sms/$', login_required(smsTest.as_view()), name='smsURL'),
+   # url(r'^login/$', loginRegister.as_view(), name='accountsLoginRegister'),
+   url(r'^login/$', loginRegisterCampaign.as_view(), name='accountsLoginURL'),
+   url(r'^register/$', loginRegisterCampaign.as_view(), {'register': True}, name='accountsRegisterURL'),
+   url(r'^print/(?P<pk>\d+)/$', login_required(printForUser.as_view()), name='print'),
+   url(r'^dashboard/$', login_required(dashboardSelector.as_view()), name='dashboardURL'),
+   url(r'^profile/$', login_required(profileUpdate.as_view()), name='profileUpdate'),
+   url(r'^', include('django.contrib.auth.urls')),
+   #url(r'^email/$', login_required(emailTest.as_view()), name='emailURL'),
+   #url(r'^sms/$', login_required(smsTest.as_view()), name='smsURL'),
 )
