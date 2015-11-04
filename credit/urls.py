632ee3612aa3cfb27a613f45c983e230f37529ca
amandegar <amandegar@gmail.com>
2015-11-04 18:46:52 +0330

    credit url add authorized

--- credit/urls.py
+++ credit/urls.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
-
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from credit.views import addCredit
 
 urlpatterns = patterns('',
-    url(r'^$', addCredit.as_view(), name='addCreditURL')
+    url(r'^$', login_required(addCredit.as_view()), name='addCreditURL')
     )
 
