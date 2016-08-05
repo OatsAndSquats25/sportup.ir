b8c4820cb24da2a49bbbdae9239377778fbc8b10
amandegar <amandegar@gmail.com>
2016-08-05 13:29:10 -0400

    update landing page for inactivitiy and add story page for reason... but story page need update..

--- flatpages/urls.py
+++ flatpages/urls.py
@@ -8,6 +8,7 @@ from views import clubRegister, contactFormView
 urlpatterns = patterns('',
     url(r'^faq/$', TemplateView.as_view(template_name='flatpages/faq.html'), name='faqURL'),
     url(r'^about/$', TemplateView.as_view(template_name='flatpages/about.html'), name='aboutURL'),
+    url(r'^story/$', TemplateView.as_view(template_name='flatpages/story.html'), name='storyURL'),
     url(r'^term/$', TemplateView.as_view(template_name='flatpages/term.html'), name='termURL'),
     url(r'^contact/$', contactFormView.as_view(), name='contactURL'),
     url(r'^register/$', clubRegister.as_view(), name='clubRegisterURL'),
